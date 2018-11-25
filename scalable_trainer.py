0b03705117d0be885d84f451105fdd7354bfe57e
souschefistry <donataus2016@gmail.com>
2018-11-25 15:57:03 -0800

    scalable batch trainer with GPU

---
+++ scalable_trainer.py
@@ -0,0 +1,264 @@
+"""
+    Copyright (c) 2018 dibghosh@stanford.edu
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the Software
+    is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import sys
+print(sys.path)
+print(sys.executable)
+import os
+import numpy as np
+import json
+import random
+import numpy as np
+import os
+import time
+import keras
+from keras.preprocessing import image
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout,Activation,Flatten
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import ResNet50
+from keras.callbacks import TensorBoard
+from keras import optimizers
+
+from keras.layers import Input
+from keras.models import Model
+from keras.utils import np_utils
+from sklearn.utils import shuffle
+
+from keras.utils.data_utils import get_file
+from keras.models import model_from_json
+from keras import backend as K
+
+import tensorflow as tf
+
+from glob import glob
+
+# lscpu Core(s) per socket: 2
+NUM_PARALLEL_EXEC_UNITS = 2
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=2, allow_soft_placement=True, device_count = {'GPU': 1, 'CPU': NUM_PARALLEL_EXEC_UNITS })
+keras.backend.set_session(sess)
+
+NUM_CLASSES = 46
+TRAIN_DATA_SIZE = 5000
+TEST_DATA_SIZE = 1000
+VAL_DATA_SIZE = 1000
+img_h = 224
+img_w = 224
+
+np.random.seed(seed=1234)
+random.seed(1234)
+
+### write both training + validation graphs in same plot
+# https://stackoverflow.com/questions/47877475/keras-tensorboard-plot-train-and-validation-scalars-in-a-same-figure
+
+class TrainValTensorBoard(TensorBoard):
+    def __init__(self, log_dir='./logs', **kwargs):
+        # Make the original `TensorBoard` log to a subdirectory 'training'
+        training_log_dir = os.path.join(log_dir, 'training')
+        super(TrainValTensorBoard, self).__init__(training_log_dir, **kwargs)
+
+        # Log the validation metrics to a separate subdirectory
+        self.val_log_dir = os.path.join(log_dir, 'validation')
+
+    def set_model(self, model):
+        # Setup writer for validation metrics
+        self.val_writer = tf.summary.FileWriter(self.val_log_dir)
+        super(TrainValTensorBoard, self).set_model(model)
+
+    def on_epoch_end(self, epoch, logs=None):
+        # Pop the validation logs and handle them separately with
+        # `self.val_writer`. Also rename the keys so that they can
+        # be plotted on the same figure with the training metrics
+        logs = logs or {}
+        val_logs = {k.replace('val_', ''): v for k, v in logs.items() if k.startswith('val_')}
+        for name, value in val_logs.items():
+            summary = tf.Summary()
+            summary_value = summary.value.add()
+            summary_value.simple_value = value.item()
+            summary_value.tag = name
+            self.val_writer.add_summary(summary, epoch)
+        self.val_writer.flush()
+
+        # Pass the remaining logs to `TensorBoard.on_epoch_end`
+        logs = {k: v for k, v in logs.items() if not k.startswith('val_')}
+        super(TrainValTensorBoard, self).on_epoch_end(epoch, logs)
+
+    def on_train_end(self, logs=None):
+        super(TrainValTensorBoard, self).on_train_end(logs)
+        self.val_writer.close()
+
+def save_model_to_disk(model, model_name="model"):
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("%s.json" % model_name, "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("%s.h5" % model_name)
+    print("Saved %s to disk" % model_name)
+    
+def load_model_from_disk(model_name):
+    # load json and create model
+    json_file = open('%s.json' % model_name, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("%s.h5" % model_name)
+    print("Loaded %s from disk" % model_name)
+    return loaded_model
+
+### network
+
+BATCH_SIZE = 128
+NUM_EPOCHS = 1
+n_H, n_W = 224, 224
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+# no augmentation for test generator
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        '../deepfashion/dataset/train/',
+        target_size=(n_H, n_W),
+        batch_size=BATCH_SIZE,
+        class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+        '../deepfashion/dataset/val/',
+        target_size=(n_H, n_W),
+        batch_size=BATCH_SIZE,
+        class_mode='categorical')
+
+test_generator = test_datagen.flow_from_directory(
+        '../deepfashion/dataset/test/',
+        target_size=(n_H, n_W),
+        batch_size=BATCH_SIZE,
+        class_mode='categorical')
+
+base_model = ResNet50(
+        weights='imagenet',
+        include_top=False,
+        input_shape=(n_H, n_H, 3))
+
+base_model.summary()
+
+last_layer = base_model.output
+# add a global spatial average pooling layer
+x = GlobalAveragePooling2D()(last_layer)
+# add fully-connected & dropout layers
+x = Dense(1024, activation='relu',name='fc-1')(x)
+# x = Dropout(0.5)(x)
+# x = Dense(256, activation='relu',name='fc-2')(x)
+# x = Dropout(0.5)(x)
+# a softmax layer for 4 classes
+predictions = Dense(NUM_CLASSES, activation='softmax',name='output_layer')(x)
+
+# this is the model we will train
+custom_resnet_model = Model(inputs=base_model.input, outputs=predictions)
+
+custom_resnet_model.summary()
+
+# first: train only the top layers (which were randomly initialized)
+# i.e. freeze all convolutional InceptionV3 layers
+for layer in custom_resnet_model.layers:
+    layer.trainable = False
+
+# custom_resnet_model.layers[-1].trainable
+
+custom_resnet_model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(), metrics=['accuracy'])
+
+# improvement #1
+# early_stopping = EarlyStopping(verbose=1, patience=40, monitor='val_loss')
+# callbacks_list = [early_stopping, tensorboard]
+
+tensorboard = TrainValTensorBoard(log_dir="./deepfashion/tboard-resnet50-logs/{}_{}_{}".format(BATCH_SIZE, NUM_EPOCHS, time.time()), write_graph=True)
+
+callbacks_list = [tensorboard]
+
+# t=time.time()
+# with tf.device('/gpu:0'):
+#     history = custom_resnet_model.fit_generator(
+#                 train_generator,
+#                 epochs=NUM_EPOCHS,
+#                 validation_data=validation_generator,
+#                 verbose=1,
+#                 callbacks=callbacks_list)
+# print('Training time (secs): %s' % (time.time() - t))
+
+# # store the model on disk
+# model_name = 'scalable_resnet50_{}_{}_{}.h5'.format(TRAIN_BATCH_SIZE, EPOCHS, time.time())
+# save_model_to_disk(custom_resnet_model, model_name)
+
+
+# print('STATIC LEARNING_PHASE = 1')
+# K.clear_session()
+# K.set_learning_phase(1)
+
+# # load saved model
+# custom_resnet_model = load_model_from_disk(model_name)
+# custom_resnet_model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(), metrics=['accuracy'])
+
+# with tf.device('/gpu:0'):
+#     (loss, accuracy) = custom_resnet_model.evaluate_generator(
+#         test_generator,
+#         verbose=1)
+
+nb_train_samples = 209222
+nb_validation_samples= 40000
+nb_test_samples = 40000
+
+t=time.time()
+with tf.device('/gpu:0'):
+    history = custom_resnet_model.fit_generator(
+                train_generator,
+                steps_per_epoch=nb_train_samples // BATCH_SIZE,
+                epochs=NUM_EPOCHS,
+                validation_data=validation_generator,
+                validation_steps=nb_validation_samples // BATCH_SIZE,
+                verbose=1,
+                callbacks=callbacks_list)
+print('Training time (secs): %s' % (time.time() - t))
+
+# store the model on disk
+model_name = 'scalable_batch_resnet50_{}_{}_{}.h5'.format(TRAIN_BATCH_SIZE, EPOCHS, time.time())
+save_model_to_disk(custom_resnet_model, model_name)
+
+print('STATIC LEARNING_PHASE = 1')
+K.clear_session()
+K.set_learning_phase(1)
+
+# load saved model
+custom_resnet_model = load_model_from_disk(model_name)
+custom_resnet_model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(), metrics=['accuracy'])
+
+with tf.device('/gpu:0'):
+    (loss, accuracy) = custom_resnet_model.evaluate_generator(
+        test_generator,
+        steps_per_epoch=nb_test_samples // BATCH_SIZE,
+        verbose=1)
+
+print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
+
